678db6ccbd3a3b0093608c6c40d0655c59d88fc2
jeremias <juansebas7011@gmail.com>
2026-02-19 15:18:12 -0500

    CRUD con ORM: Crear, Leer, Actualizar y Eliminar Datos

--- curso-nodejs-postgres/services/user.service.js
+++ curso-nodejs-postgres/services/user.service.js
@@ -6,7 +6,8 @@ class UserService {
   constructor() {}
 
   async create(data) {
-    return data;
+    const newUser = await models.User.create(data);
+    return newUser;
   }
 
   async find() {
@@ -16,18 +17,25 @@ class UserService {
   }
 
   async findOne(id) {
-    return { id };
+
+    const user =await models.User.findByPk(id);
+
+    if(!user){
+    throw  boom.notFound('user not found ')
+    }
+    return user;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const user = await this.findOne(id);
+    const rta = await user.update(changes);
+    return rta;
   }
 
   async delete(id) {
-    return { id };
+    const user =await models.User.findByPk(id);
+    await user.destroy();
+    return {id};
   }
 }
 
