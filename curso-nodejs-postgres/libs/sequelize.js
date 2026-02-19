4b85a961e966650760dd48e59c86d2d4ed77aa33
jeremias <juansebas7011@gmail.com>
2026-02-19 12:24:21 -0500

    Creación y Gestión de Modelos con ORM en Node.js

--- curso-nodejs-postgres/libs/sequelize.js
+++ curso-nodejs-postgres/libs/sequelize.js
@@ -1,6 +1,7 @@
  const { Sequelize } = require('sequelize');
 
 const { config } = require('./../config/config');
+const setupModels = require('./../db/dbmodels');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
@@ -10,5 +11,14 @@ const sequelize = new Sequelize(URI, {
   dialect: 'postgres',
   logging: true,
 });
+setupModels(sequelize);
 
-module.exports = sequelize;
+sequelize.sync();//
+
+// Verificar que el modelo User esté registrado
+console.log('Modelos registrados:', Object.keys(sequelize.models));
+
+module.exports = {
+  sequelize,
+  models: sequelize.models,
+};
