1d7a9f09f524661cd5dd6ab14f94631b64bd7179
jeremias <juansebas7011@gmail.com>
2026-02-18 20:57:05 -0500

    hice ajustes para colocar un URI con la info de la db

--- curso-nodejs-postgres/libs/postgres.pool.js
+++ curso-nodejs-postgres/libs/postgres.pool.js
@@ -1,16 +1,21 @@
-
-
 //este de aqui exporta el pool
-const {Pool} = require('pg');
+const {Pool} = require ('pg');
+
+const {config} = require('./../config/config');
+
+const USER = encodeURIComponent ( config.dbUser);
+const PASSWORD= encodeURIComponent( config.dbPassword);
+const URI = `postges:// ${USER}:${PASSWORD}@ ${config.dbHost}/${config.dbName}`;
 
 const pool= new Pool({
 
-host: "localhost",
-port: 5432,
-user: "juan",
-password: "juan3",
-database: "My_store"
+ connectionString: URI
 
 });
 
 module.exports = pool ;
+
+// Configura un Pool de conexiones usando una URI dinámica.
+// Sirve para centralizar los datos en una sola dirección (URL),
+// asegurar que caracteres especiales en la clave no rompan la conexión
+// y preparar la app para subirla a servidores reales (nube).
